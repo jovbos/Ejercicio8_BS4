d7c1496173700c81b7e273a69e8dd2023a36c7bd
Joaquín Olmedo Villodres <joaquin.olmedo@bosonit.com>
2022-05-24 17:00:12 +0200

    First commit

---
+++ src/main/java/paquete/DemoApplication.java
@@ -0,0 +1,16 @@
+package paquete;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
