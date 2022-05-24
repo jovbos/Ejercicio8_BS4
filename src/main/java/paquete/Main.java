5b136b65a568a2bf7d63c2679929f0850573b161
Joaquín Olmedo Villodres <joaquin.olmedo@bosonit.com>
2022-05-24 17:02:54 +0200

    Nombre main class actualizado

--- src/main/java/paquete/Main.java
+++ src/main/java/paquete/Main.java
@@ -1,16 +1,13 @@
 package paquete;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.annotation.PostConstruct;
-
 @SpringBootApplication
-public class DemoApplication {
+public class Main {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Main.class, args);
 	}
 
 }
